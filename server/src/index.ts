3d5d7946dd800d21e313ce18ed316fe0c952c681
Muhammad Fauzan <mfauzan@192.168.1.5>
2025-06-17 00:08:43 +0700

    set docker for server and client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import { ItemType, PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { asyncHandler } from "./utils/asyncHandler";
 
 const prisma = new PrismaClient()
 const app = express()
@@ -9,53 +10,66 @@ const port = 3000
 
 app.use(express.json())
 
-app.post('/items', async (req: Request, res: Response) => {
-  const { name, parentId, type } = req.body;
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) => {
+    res.send("Hello World");
+  })
+);
 
-  if (!name) {
-    return res.status(400).json({ message: "Item name is required" });
-  }
+app.post(
+  '/items',
+  asyncHandler(async (req: Request, res: Response) => {
+    const { name, parentId, type } = req.body;
 
-  if (!Object.values(ItemType).includes(type as ItemType)) {
-    return res.status(400).json({ message: "Item type name must be 'folder' or 'file'" });
-  }
-  if (type === 'file' && !parentId) {
-    return res.status(400).json({ message: "Item type 'file' must have parent id'" });
-  }
+    if (!name) {
+      return res.status(400).json({ message: "Item name is required" });
+    }
 
-  try {
-    const folder = await prisma.item.create({
-      data: {
-        name,
-        parentId: parentId ?? null,
-        type,
-      },
+    if (!Object.values(ItemType).includes(type as ItemType)) {
+      return res.status(400).json({ message: "Item type name must be 'folder' or 'file'" });
+    }
+    if (type === 'file' && !parentId) {
+      return res.status(400).json({ message: "Item type 'file' must have parent id'" });
+    }
+
+    try {
+      const folder = await prisma.item.create({
+        data: {
+          name,
+          parentId: parentId ?? null,
+          type,
+        },
+      });
+
+      res.status(201).json(folder);
+    } catch (error) {
+      console.error("Error creating item:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  })
+);
+
+app.get(
+  '/items',
+  asyncHandler(async (req: Request, res: Response) => {
+    const { type } = req.query;
+    const typeStr = String(type);
+
+    if (!Object.values(ItemType).includes(typeStr as ItemType)) {
+      return res.status(400).json({ message: "Item type name must be 'folder' or 'file'" });
+    }
+
+    const rootItems = await prisma.item.findMany({
+      where: {
+        parentId: null,
+        type: typeStr as ItemType,
+      }
     });
 
-    res.status(201).json(folder);
-  } catch (error) {
-    console.error("Error creating item:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-app.get('/items', async (req: Request, res: Response) => {
-  const { type } = req.query;
-  const typeStr = String(type);
-
-  if (!Object.values(ItemType).includes(typeStr as ItemType)) {
-    return res.status(400).json({ message: "Item type name must be 'folder' or 'file'" });
-  }
-
-  const rootItems = await prisma.item.findMany({
-    where: {
-      parentId: null,
-      type: typeStr as ItemType,
-    }
-  });
-
-  res.json(rootItems);
-});
+    res.json(rootItems);
+  })
+);
 
 app.get('/items/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
